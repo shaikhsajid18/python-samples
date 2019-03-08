4baa12d382d4d73816c4a3cd2fd59452ab9546bf
sshaikh <sshaikh@bjs.com>
2019-03-08 13:46:31 -0500

    Added initial code

---
+++ unencrypted-volume-details.py
@@ -0,0 +1,19 @@
+import boto3
+from botocore.client import Config
+
+profiles = ['genprod']  # , 'gendev', 'genprod', 'ecommdev', 'ecommqa', 'ecommprod', 'sapdev', 'sapqa', 'sapprod', 'security']
+config = Config(region_name='us-east-1')
+
+def volumedetails():
+    for profile in profiles:
+        print(profile)
+        session = boto3.Session(profile_name=profile)
+        ec2_resource = session.resource('ec2', config=config)
+        for volume in ec2_resource.volumes.filter(Filters=[{'Name': 'encrypted', 'Values': ['false',]},
+                                                           {'Name': 'status', 'Values': ['in-use',]}]):
+            if volume.attachments[-1]['Device'] not in ['/dev/xvda', '/dev/sda1']:
+                print(volume.volume_id, volume.attachments[-1]['Device'])
+
+
+if __name__ == '__main__':
+    volumedetails()
